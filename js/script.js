60913701e5d1f073959c20c68a308c50d996fb18
mimibun <166717259+mimibun@users.noreply.github.com>
2024-05-04 14:59:44 +0200

    feat(script.js): add random number generator

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 function getComputerChoice() {
     const randomNum = Math.floor(Math.random() * 3);
-    console.log(random);
+    let choiceString = '';
 
-    return handChoice;
+    switch (randomNum) {
+        case 0:
+            choiceString = 'Rock';
+            break;
+        case 1:
+            choiceString = 'Paper';
+            break;
+        case 2:
+            choiceString = 'Scissors';
+            break;
+    }
+
+    return choiceString;
 }
 
 getComputerChoice();
