14e8b07d1c568f8ee30b1a1dca74917b6bafeb06
mimibun <166717259+mimibun@users.noreply.github.com>
2024-05-04 17:00:50 +0200

    feat(script.js): add playRound func

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let computerScore = 0;
-let playerScore = 0;
+let humanScore = 0;
 
 
 function getComputerChoice() {
@@ -33,6 +33,32 @@ function getPlayerChoice() {
     }
 }
 
-console.log("player: " + getPlayerChoice());
-
-console.log("pomputer: " + getComputerChoice());
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("It's a TIE!");
+    } else if (humanChoice === "Rock") {
+        if (computerChoice === "Scissors") {
+            humanScore++;
+            console.log(`You win! Computer loses! Current score: You:${humanScore} - Computer:${computerScore}`)
+        } else {
+            computerScore++;
+            console.log(`You lose(lol what a loser, lol get rekt noob)! Computer wins! Current score: You:${humanScore} - Computer:${computerScore}`)
+        }
+    } else if (humanChoice === "Paper") {
+        if (computerChoice === "Rock") {
+            humanScore++;
+            console.log(`You win! (Crazy! you really showed him, good job uwu) Computer loses! Current score: You:${humanScore} - Computer:${computerScore}`)
+        } else {
+            computerScore++;
+            console.log(`I'm sorry to have to inform you, but unfortunately you lost this round :c Better luck next time! Computer wins! Current score: You:${humanScore} - Computer:${computerScore}`)
+        }
+    } else {
+        if (computerChoice === "Paper") {
+            humanScore++;
+            console.log(`Me thinkies you won uwu! Computer loses! Current score: You:${humanScore} - Computer:${computerScore}`)
+        } else {
+            computerScore++;
+            console.log(`take the L + ratio + sit + who asked + bozo! Computer wins! Current score: You:${humanScore} - Computer:${computerScore}`)
+        }
+    }
+}
